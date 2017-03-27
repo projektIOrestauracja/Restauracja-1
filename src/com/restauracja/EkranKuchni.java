63fcad222a7866a423957d1901d0ce732bb99fd1
damianw345 <damianw345@gmail.com>
2017-03-27 18:01:07 +0200

    Dodano mozliwosc dodawania dan do menu

--- src/com/restauracja/EkranKuchni.java
+++ src/com/restauracja/EkranKuchni.java
@@ -2,20 +2,37 @@ package com.restauracja;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by dw on 24.03.17.
  */
 public class EkranKuchni {
+    private Kuchnia kuchnia;
     private JPanel panelEkranuKuchni;
     private JTextField tuMaBycTabelaTextField;
+    private JButton dodajDanieButton;
+    private JTextField nazwaDaniaField;
+    private JTextField cenaDaniaField;
 
     public EkranKuchni() {
+        kuchnia = new Kuchnia();
+
         JFrame frame = new JFrame("Ekran kuchni");
         frame.setContentPane(panelEkranuKuchni);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
         frame.setPreferredSize(new Dimension(650, 650));
+
+        dodajDanieButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String nazwaDania = nazwaDaniaField.getText();
+                String cenaDania = cenaDaniaField.getText();
+                Restauracja.restauracja.getMenuRestauracji().dodajNoweDanie(nazwaDania, Double.parseDouble(cenaDania));
+            }
+        });
     }
 }
--- src/com/restauracja/MenuRestauracji.java
+++ src/com/restauracja/MenuRestauracji.java
@@ -15,10 +15,13 @@ public class MenuRestauracji {
     MenuRestauracji() {
         czytnikPlikow = new CzytnikPlikow();
         listaDan = new ArrayList<>();
+
+        //odczytanie poczatkowego stanu Menu
+        odczytajDaniaZPlikuDoListy();
     }
 
     public void odczytajDaniaZPlikuDoListy() {
-        String menu = czytnikPlikow.czytajPlik();
+        String menu = czytnikPlikow.czytajPlik("menu.txt");
         Scanner scanner = new Scanner(menu);
 
         while (scanner.hasNext()) {
@@ -26,25 +29,38 @@ public class MenuRestauracji {
             int id = Integer.parseInt(idStr);
             String nazwa = scanner.next();
             String cenaString = scanner.next();
-            float cena = Float.parseFloat(cenaString);
+            double cena = Double.parseDouble(cenaString);
 
             Danie danie = new Danie(id, nazwa, cena);
             listaDan.add(danie);
         }
 
+        //DEBUGOWANIE
 //        for (Danie d : listaDan) {
-//            System.out.println(d.getIdDania());
-//            System.out.println(d.getNazwaDania());
+//            System.out.print(d.getIdDania() + " ");
+//            System.out.print(d.getNazwaDania() + " ");
 //            System.out.println(d.getCenaDania());
 //        }
 
     }
 
-    private void dodajNoweDanieDoPliku() {
+    public void dodajNoweDanie(String nazwaDania, double cenaDania) {
 
-    }
+        Danie danie = new Danie(nazwaDania, cenaDania);
+        listaDan.add(danie);
 
-    private void aktualizujListeDan() {
+        String idString = Integer.toString(danie.getIdDania());
 
+        String cenaString = Double.toString(cenaDania);
+
+        czytnikPlikow.zapiszPlik("menu.txt", idString + " " + nazwaDania + " " + cenaString);
+
+        //DEBUGOWANIE
+//        for (Danie d : listaDan) {
+//            System.out.print(d.getIdDania() + " ");
+//            System.out.print(d.getNazwaDania() + " ");
+//            System.out.println(d.getCenaDania());
+//        }
     }
 }
+
